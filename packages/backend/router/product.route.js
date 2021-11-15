805f3b761d9aca82e8adbf186fb6496f49c42ca5
Nhat Huy Le <65520688+huylee99@users.noreply.github.com>
2021-11-14 20:43:54 -0800

    feat: 🎸 get products with filters and pagination (#21)

--- packages/backend/router/product.route.js
+++ packages/backend/router/product.route.js
@@ -3,8 +3,7 @@ const productController = require('../controllers/product.controller');
 
 const router = express.Router();
 
-router.get('/getAll', productController.getAll);
-router.get('/getByCategory', productController.getProductsByCategory);
+router.get('/getProducts', productController.getAll);
 router.post('/create', productController.createProduct);
 
 module.exports = router;
