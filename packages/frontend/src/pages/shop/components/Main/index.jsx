c1ef35b8a32b19faaf05011db6ceddceced404ef
Nhat Huy Le <65520688+huylee99@users.noreply.github.com>
2021-11-15 23:48:08 -0800

    feat: 🎸 integrated productsAPI into Shop with filters (#24)

--- packages/frontend/src/pages/shop/components/Main/index.jsx
+++ packages/frontend/src/pages/shop/components/Main/index.jsx
@@ -1,18 +1,30 @@
+import { useQuery } from '@/hooks/useQuery';
+
 import SectionDivider from '@/components/SectionDivider';
 import Container from '@/components/Container';
 import Filters from '../Filters';
 import Shop from '../Shop';
 
 const MainShop = () => {
+  let [query, setQuery] = useQuery();
+
+  if (!query) {
+    query = { limit: 12, categories: '', rating: '', price: '' };
+  }
+
+  const handleChange = event => {
+    setQuery({ ...query, [event.target.name]: event.target.value });
+  };
+
   return (
     <SectionDivider size='lg'>
       <Container size='md'>
         <div className='flex gap-10 w-full'>
           <div className='max-w-[25%] w-[25%]'>
-            <Filters />
+            <Filters query={query} handleChange={handleChange} />
           </div>
           <div className='max-w-[75%] w-[75%]'>
-            <Shop />
+            <Shop query={query} handleChange={handleChange} />
           </div>
         </div>
       </Container>
--- packages/frontend/src/pages/shop/components/Shop/index.jsx
+++ packages/frontend/src/pages/shop/components/Shop/index.jsx
@@ -1,35 +1,92 @@
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+
+import { stringifyQuery } from '@/helpers/URLHelpers';
+import productRequest from '@/api/productAPI';
+
 import ProductCard from '@/components/ProductCard';
 
-const Shop = () => {
+const Shop = ({ query, handleChange }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const cancelToken = axios.CancelToken;
+    const source = cancelToken.source();
+
+    let queries = query ? stringifyQuery(query) : '';
+    fetchData(queries);
+    return () => source.cancel();
+  }, [query]);
+
+  const fetchData = async queries => {
+    setLoading(true);
+    try {
+      const response = await productRequest.getProducts(queries);
+      setData(response.data);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const idxTo = data.currentPage * query.limit;
+  const idxFrom = Math.ceil(idxTo / query.limit);
+
   return (
     <div>
       <div className='flex items-center justify-between mb-5'>
         <h2 className='text-dark font-bold text-3xl'>Shop</h2>
         <span className='font-semibold text-base text-gray-400'>
-          Showing 1-15 of 33 item(s)
+          {/* {`${totalItems > 0 ? {'Showing'} ${idxFrom}-${idxTo > data.totalItems ? data.totalItems : idxTo}'of'} : ''`} */}
+          Showing{' '}
+          {data.totalItems > 0
+            ? `${idxFrom}-${
+                idxTo > data.totalItems ? data.totalItems : idxTo
+              } of`
+            : ''}{' '}
+          {data.totalItems} item(s)
         </span>
       </div>
       <div className='p-2 bg-gray-200 rounded-3xl flex justify-between items-center mb-5'>
-        <div className='px-4 py-1 rounded-3xl bg-gray-50 text-sm font-semibold text-gray-400'>
-          Show 15
-        </div>
+        <select
+          className='py-1 px-4 appearance-none focus:outline-none rounded-3xl bg-gray-50 text-sm font-semibold text-gray-400 cursor-pointer'
+          name='limit'
+          onChange={handleChange}
+          value={query.limit}
+        >
+          <option value='12'>12 per page</option>
+          <option value='24'>24 per page</option>
+        </select>
 
         <div className='px-4 py-1 rounded-3xl bg-gray-50 text-sm font-semibold text-gray-400'>
           Default sorting
         </div>
       </div>
-      <div className='flex flex-wrap gap-3 items-stretch'>
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-        <ProductCard width='w-[24%]' />
-      </div>
+      {loading ? (
+        'Loading...'
+      ) : (
+        <div className='flex flex-wrap gap-3 items-stretch'>
+          {data.products.length === 0
+            ? 'There is no product...'
+            : data.products.map(product => (
+                <ProductCard
+                  key={product._id}
+                  product={product}
+                  width='w-[24%]'
+                />
+              ))}
+        </div>
+      )}
     </div>
   );
+};
+
+Shop.propTypes = {
+  query: PropTypes.any,
+  handleChange: PropTypes.func,
 };
 
 export default Shop;
