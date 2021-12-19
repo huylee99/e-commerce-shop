9a57f38a86ed12a0be23aaa1fc80ea41ca25459c
huyle99 <huy.le7@pcc.edu>
2021-12-18 16:42:00 -0800

    feat: 🎸 order history section with pagination

--- packages/backend/router/order.route.js
+++ packages/backend/router/order.route.js
@@ -5,7 +5,8 @@ const { tokenVerification } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/create', tokenVerification, orderController.createOrder);
-router.get('/get', orderController.getOrder);
 router.get('/verifyOrder', tokenVerification, orderController.checkOrder);
+router.get('/getAllOrders', tokenVerification, orderController.getAllOrder);
+router.get('/get', orderController.getOrder);
 
 module.exports = router;
--- packages/frontend/src/pages/user/components/OrderHistory/index.jsx
+++ packages/frontend/src/pages/user/components/OrderHistory/index.jsx
@@ -1,65 +1,127 @@
+import { useState, useEffect } from 'react';
+import orderRequest from '../../../../api/orderAPI';
+import responseMessage from '../../../../constant/responseMessage';
+import { dateFormat } from '../../../../helpers/dateFormat';
+import { useQuery } from '../../../../hooks/useQuery';
+
+import Pagination from '../../../../components/Pagination';
+
 const OrderHistory = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  let [query, setQuery] = useQuery();
+
+  if (!query) {
+    query = { page: 1 };
+  }
+
+  const handleSetQuery = pageNum => {
+    setQuery({ page: pageNum });
+  };
+
+  useEffect(() => {
+    if (query.page) {
+      fetchOrders();
+    }
+  }, [query.page]);
+
+  const fetchOrders = async () => {
+    setIsLoading(true);
+    const response = await orderRequest.getAllOrders(query.page);
+    const { data, message } = response.data;
+    if (message === responseMessage.GET_SUCCESSFULLY) {
+      setData(data);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <h2 className='font-bold text-2xl mb-4'>Order History</h2>
-      <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
-        <div className='py-2 inline-block min-w-full sm:px-6 lg:px-8'>
-          <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
-            <table className='min-w-full divide-y divide-gray-200'>
-              <thead className='bg-gray-100'>
-                <tr>
-                  <th
-                    scope='col'
-                    className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
-                  >
-                    Order ID
-                  </th>
-                  <th
-                    scope='col'
-                    className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
-                  >
-                    Date
-                  </th>
-                  <th
-                    scope='col'
-                    className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
-                  >
-                    Status
-                  </th>
-                </tr>
-              </thead>
-              <tbody className='bg-white divide-y divide-gray-200'>
-                <tr>
-                  <td className='px-6 py-4 whitespace-nowrap'>
-                    <div className='text-base font-medium text-gray-900 underline hover:text-blue-500 cursor-pointer'>
-                      1234
-                    </div>
-                  </td>
-                  <td className='px-6 py-4 whitespace-nowrap'>
-                    <div className='text-base font-medium text-gray-900'>
-                      10-24-2021
-                    </div>
-                  </td>
-                  <td className='px-6 py-4 whitespace-nowrap'>
-                    <span className='px-3 py-1 inline-flex text-sm leading-5 font-bold rounded-full bg-green-100 text-green-800'>
-                      Delivered
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <div className='bg-white py-3 border-t border-gray-200 px-6 w-full'>
-              <div className='flex items-center justify-between w-full'>
-                <div>
+      {isLoading ? (
+        '...Loading'
+      ) : data.orders && data.orders.length > 0 ? (
+        <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
+          <div className='py-2 inline-block min-w-full sm:px-6 lg:px-8'>
+            <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+              <table className='min-w-full divide-y divide-gray-200'>
+                <thead className='bg-gray-100'>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
+                    >
+                      Order ID
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-sm font-bold text-gray-500 uppercase tracking-wider'
+                    >
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className='bg-white divide-y divide-gray-200'>
+                  {data.orders.map(({ _id, status, orderId, createdAt }) => (
+                    <tr key={_id}>
+                      <td className='px-6 py-4 whitespace-nowrap'>
+                        <a
+                          href={`/tracking?id=${orderId}`}
+                          target='_blank'
+                          rel='noreferrer'
+                          className='text-base font-medium text-gray-900 underline hover:text-blue-500 cursor-pointer'
+                        >
+                          {orderId}
+                        </a>
+                      </td>
+                      <td className='px-6 py-4 whitespace-nowrap'>
+                        <span className='text-base font-medium text-gray-900'>
+                          {dateFormat(createdAt)}
+                        </span>
+                      </td>
+                      <td className='px-6 py-4 whitespace-nowrap'>
+                        <span className='px-3 py-1 inline-flex text-sm leading-5 font-bold rounded-full bg-green-100 text-green-800'>
+                          {status}
+                        </span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div className='bg-white py-3 border-t border-gray-200 px-6 w-full'>
+                <div className='flex items-center justify-between w-full'>
                   <p className='text-sm font-bold text-gray-500'>
-                    Showing 1 to 1 of 1 results
+                    {` Showing ${5 * (data.currentPage - 1) + 1} to
+                      ${
+                        data.currentPage * 5 > data.totalItems
+                          ? data.totalItems
+                          : data.currentPage * 5
+                      }
+                      of ${data.totalItems} results`}
                   </p>
+                  {data.totalPages > 1 ? (
+                    <Pagination
+                      data={{
+                        totalPages: data.totalPages,
+                        currentPage: data.currentPage,
+                      }}
+                      setPage={handleSetQuery}
+                    />
+                  ) : null}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div>No orders</div>
+      )}
     </>
   );
 };
